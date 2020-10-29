ca3db7ef9933e9013d3287fbc6db9510b7274368
David Lajos Mihalovits <dmihalovits@gmail.com>
2020-10-29 13:14:21 +0700

    new

---
+++ index.js
@@ -0,0 +1,16 @@
+const express = require("express");
+const cors = require("cors");
+const bodyParser = require("body-parser");
+const db = require("./config/db");
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+db.authenticate().then(() => console.log("Postgres database connected."));
+db.sync();
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => console.log(`Server started on port ${PORT}.`));
